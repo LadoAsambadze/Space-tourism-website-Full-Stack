75ee33ec9bdb8ba08b7b5a78f182b20e09335019
Lado Asambadze <lado.asambadze1@gmail.com>
2023-06-09 23:18:53 +0400

    feat: creat main page for all resolutions on route (/)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import MainPage from "./pages/MainPage";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
