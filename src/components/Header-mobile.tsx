065621731a55a56361d81efa9400c05e08b4a32f
Lado Asambadze <lado.asambadze1@gmail.com>
2023-06-09 23:30:45 +0400

    fix: mistakes with address of photos

--- src/components/Header-mobile.tsx
+++ src/components/Header-mobile.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="w-full absolute px-6 pt-6 flex flex-row justify-between items-center md:hidden">
-      <img src="shared/logo.svg" alt="logo" />
+      <img src="share/logo.svg" alt="logo" />
       <img
         src="shared/icon-hamburger.svg"
         alt="hamburger icon"
--- src/components/Header-tablet.tsx
+++ src/components/Header-tablet.tsx
@@ -1,7 +1,7 @@
 export default function HeaderTablet() {
   return (
     <div className="sm:hidden  absolute w-full  flex-row md:flex md:justify-between lg:hidden">
-      <img src="shared/logo.svg" alt="logo" className="py-6 pl-10" />
+      <img src="share/logo.svg" alt="logo" className="py-6 pl-10" />
       <div className="w-full ml-[230px]  py-10 px-12 flex flex-row  items-center justify-between backdrop-blur-custom bg-divBack">
         <span className="text-white text-xs leading-4  tracking-[2.3px]">
           HOME
